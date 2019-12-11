14c719287213021bb794dc41b1f4d6e64993dba5
Sooyong  Kim <30457077+dev-wd@users.noreply.github.com>
2019-12-11 20:37:12 +0900

    Update FruitEX2ViewController.swift

--- example2/viewcontroller/FruitEX2ViewController.swift
+++ example2/viewcontroller/FruitEX2ViewController.swift
@@ -7,52 +7,81 @@ class FruitEX2ViewController: UIViewController , UITableViewDelegate{
     @IBOutlet weak var nextPageButton: UIButton!
     
     
-    var firstFruitDic: BehaviorRelay<[[String : String]]> = BehaviorRelay(value:[["number":"1.","name":""],["number":"2.","name":""],["number":"3.","name":""],["number":"4.","name":""],["number":"5.","name":""],["number":"6.","name":""],["number":"7.","name":""],["number":"8.","name":""]])
+    var firstFruitDic: BehaviorRelay<[[String : String]]> =
+        BehaviorRelay(value:
+            [["number":"1.","name":""],
+             ["number":"2.","name":""],
+             ["number":"3.","name":""],
+             ["number":"4.","name":""],
+             ["number":"5.","name":""],
+             ["number":"6.","name":""],
+             ["number":"7.","name":""],
+             ["number":"8.","name":""]])
     
-    var updatedFruitDic: BehaviorRelay<[[String : String]]> = BehaviorRelay(value:[["number":"1.","name":""],["number":"2.","name":""],["number":"3.","name":""],["number":"4.","name":""],["number":"5.","name":""],["number":"6.","name":""],["number":"7.","name":""],["number":"8.","name":""]])
+    var updatedFruitDic: BehaviorRelay<[[String : String]]> =
+        BehaviorRelay(value:
+        [["number":"1.","name":""],
+         ["number":"2.","name":""],
+         ["number":"3.","name":""],
+         ["number":"4.","name":""],
+         ["number":"5.","name":""],
+         ["number":"6.","name":""],
+         ["number":"7.","name":""],
+         ["number":"8.","name":""]])
     
-       var disposeBag = DisposeBag()
+    var disposeBag = DisposeBag()
     var temDic : [[String: String]] = []
     
-       override func viewDidLoad() {
-           super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-         InputTableView()
-        ButtonTapped()
-       }
-    
-    func ButtonTapped() {
-        
-        // Press nextpage button.
-        nextPageButton.rx.tap.asDriver().drive(onNext: {
-            self.performSegue(withIdentifier: "showNextPage", sender: self)
-            print("buttontapp fruid dic : ",self.firstFruitDic.value)
-            
-           
-            }).disposed(by: disposeBag)
+        inputTableView()
+        buttonTapped()
     }
     
-    func InputTableView() {
+    func buttonTapped() {
+        
+        // Press nextpage button.
+        nextPageButton.rx
+            .tap
+            .asDriver()
+            .drive(onNext: {
+                self.performSegue(withIdentifier: "showNextPage", sender: self)
+                print("buttontapp fruid dic : ",self.firstFruitDic.value)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func inputTableView() {
         
         temDic = self.firstFruitDic.value
         // Set tableview delegate. (for setting table view cell height)
-        tableRef.rx.setDelegate(self).disposed(by: disposeBag)
+        tableRef.rx
+            .setDelegate(self)
+            .disposed(by: disposeBag)
         
         // Bind fruit dictionary and tableview.
-        firstFruitDic.asObservable().bind(to: tableRef.rx.items(cellIdentifier: "Cell", cellType: FruitEX2Cell.self))
-        {index, element, cell in
-            
-        // Write number for cell textfield.
-        cell.fruitNumber.text = element["number"]
-            
-        // Update Dictionary from cell textfield.
-        cell.fruitTextField.rx.text.orEmpty.asDriver().drive(onNext: { cellvalue in
-            self.temDic[index]["name"] = cellvalue
-            self.updatedFruitDic.accept(self.temDic)
-
-            }).disposed(by: cell.bag)
-           
-        }.disposed(by: disposeBag)
+        firstFruitDic.asObservable()
+            .bind(to: tableRef.rx
+                .items(cellIdentifier: "Cell", cellType: FruitEX2Cell.self))
+            {index, element, cell in
+                
+                // Write number for cell textfield.
+                cell.fruitNumber.text = element["number"]
+                
+                // Update Dictionary from cell textfield.
+                
+                cell.fruitTextField.rx
+                    .text
+                    .orEmpty
+                    .asObservable()
+                    .bind(onNext: { cellvalue in
+                        self.temDic[index]["name"] = cellvalue
+                        self.updatedFruitDic.accept(self.temDic)
+                    })
+                    .disposed(by: cell.bag)
+        }
+        .disposed(by: disposeBag)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -62,6 +91,7 @@ class FruitEX2ViewController: UIViewController , UITableViewDelegate{
     // For showing result, pass parameter to next page.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let param = segue.destination as! FruitEX2NextPageViewController
-            param.showDic = updatedFruitDic
+        param.showDic = updatedFruitDic
     }
 }
+
