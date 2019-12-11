5f84389588b9de4a7b45e6aeb509bc6b8c2b0339
Sooyong  Kim <30457077+dev-wd@users.noreply.github.com>
2019-12-11 20:37:41 +0900

    Update FruitEX3ViewController.swift

--- example3/viewcontroller/FruitEX3ViewController.swift
+++ example3/viewcontroller/FruitEX3ViewController.swift
@@ -7,39 +7,50 @@ class FruitEX3ViewController: UIViewController, UITableViewDelegate {
     
     @IBOutlet weak var tableRef: UITableView!
     
-    var fruitDic: BehaviorRelay<[[String : String]]> = BehaviorRelay(value:[["name":"apple"],["name":"banana"], ["name":"cherries"],["name":"grapes"],["name":"lemon"],["name":"orange"],["name":"strawberry"],["name":"tomato"]])
+    var fruitDic: BehaviorRelay<[[String : String]]> = BehaviorRelay(value:
+        [["name":"apple"],
+         ["name":"banana"],
+         ["name":"cherries"],
+         ["name":"grapes"],
+         ["name":"lemon"],
+         ["name":"orange"],
+         ["name":"strawberry"],
+         ["name":"tomato"]])
     
     var disposeBag = DisposeBag()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        InputTableView()
-        }
+        inputTableView()
+    }
     
-    func InputTableView() {
+    func inputTableView() {
         
         // Set tableview delegate. (for setting table view cell height)
-        tableRef.rx.setDelegate(self).disposed(by: disposeBag)
+        tableRef.rx
+            .setDelegate(self)
+            .disposed(by: disposeBag)
         
         // Bind fruit dictionary and tableview.
-        fruitDic.asObservable().bind(to: tableRef.rx.items(cellIdentifier: "Cell", cellType: FruitEX3Cell.self))
-        {index, element, cell in
-            
-            // Write image, name for cell label.
-            cell.fruitImage.image = UIImage(named: element["name"]!)
-            cell.fruitNameLabel.text = element["name"]
-           
-            // Remove cell by cancel button.
-            cell.cancelButton.rx.tap
-             .subscribe(onNext: { [weak self] in
-             let curIndex = index
-                 var tempDic = self!.fruitDic.value
-            tempDic.remove(at: curIndex)
-                 self!.fruitDic.accept(tempDic)
-                 print("ViewModel.QRDid: ",self!.fruitDic.value)
-             }).disposed(by: cell.bag)
-            
+        fruitDic.asObservable()
+            .bind(to: tableRef.rx
+                .items(cellIdentifier: "Cell", cellType: FruitEX3Cell.self))
+            {index, element, cell in
+                
+                // Write image, name for cell label.
+                cell.fruitImage.image = UIImage(named: element["name"]!)
+                cell.fruitNameLabel.text = element["name"]
+                
+                // Remove cell by cancel button.
+                cell.cancelButton.rx.tap
+                    .subscribe(onNext: { [weak self] in
+                        let curIndex = index
+                        var tempDic = self!.fruitDic.value
+                        tempDic.remove(at: curIndex)
+                        self!.fruitDic.accept(tempDic)
+                        print("ViewModel.QRDid: ",self!.fruitDic.value)
+                    })
+                    .disposed(by: cell.bag)
         }.disposed(by: disposeBag)
         
         tableRef.tableFooterView = UIView()
@@ -50,5 +61,5 @@ class FruitEX3ViewController: UIViewController, UITableViewDelegate {
         return 120
     }
 }
-    
+
 
